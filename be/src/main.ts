6e5e3f6c6dfc2ddf806c4f247cf2e4d7c211a30a
Otzzu <mesachharmasendro@gmail.com>
2025-11-24 12:35:22 +0700

    chore: init be

---
+++ be/src/main.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).send('hello world');
+});
+
+const server = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Backend running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+};
+
+server();
